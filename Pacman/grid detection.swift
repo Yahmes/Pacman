1bd71919fa1c4f95cc1b10c727ec49d570003fef
Horge <mattk@ZPR-L0007089.local>
2017-12-05 09:19:03 -0500

    y-axis partly done

--- Pacman/grid detection.swift
+++ Pacman/grid detection.swift
@@ -12,10 +12,102 @@ import GameplayKit
 
 func GridDetection(coordinates: CGPoint) -> CGPoint {
     var tile: CGPoint
-    var gridX: Double = 27.4285714286
-    var gridY: Double = 27.428571424
     
     if coordinates.x > -384 && coordinates.x < -356.5714286 {
+        if coordinates.y < 512 && coordinates.y > 484.5714286 {
+            
+        }
+        if coordinates.y < 484.5714286 && coordinates.y > 457.1428572{
+            
+        }
+        if coordinates.y < 457.1428572 && coordinates.y > 429.7142857 {
+            
+        }
+        if coordinates.y < 429.7142857 && coordinates.y > 402.2857143 {
+            
+        }
+        if coordinates.y < 402.2857143 && coordinates.y > 374.8571429 {
+            
+        }
+        if coordinates.y < 374.8571429 && coordinates.y > 347.4285715 {
+            
+        }
+        if coordinates.y < 347.4285715 && coordinates.y > 320 {
+            
+        }
+        if coordinates.y < 320 && coordinates.y > 292.5714286 {
+            
+        }
+        if coordinates.y < 292.5714286 && coordinates.y > 265.1428572 {
+            
+        }
+        if coordinates.y < 265.1428572 && coordinates.y > 237.7142858 {
+            
+        }
+        if coordinates.y < 237.7142858 && coordinates.y > 210.2857143 {
+            
+        }
+        if coordinates.y < 210.2857143 && coordinates.y > 182.8571429 {
+            
+        }
+        if coordinates.y < 182.8571429 && coordinates.y > 155.4285715 {
+            
+        }
+        if coordinates.y < 155.4285715 && coordinates.y > 128.0000001 {
+            
+        }
+        if coordinates.y < 128.0000001 && coordinates.y > 100.5714286 {
+            
+        }
+        if coordinates.y < 100.5714286 && coordinates.y > 73.14285722 {
+            
+        }
+        if coordinates.y < 73.14285722 && coordinates.y > 45.71428579 {
+            
+        }
+        if coordinates.y < 45.71428579 && coordinates.y > 18.28571437 {
+            
+        }
+        if coordinates.y < 18.28571437 && coordinates.y > -9.142857056 {
+            
+        }
+        if coordinates.y < -9.142857056 && coordinates.y > -36.57142848 {
+            
+        }
+        if coordinates.y < -36.57142848 && coordinates.y > -63.99999999 {
+            
+        }
+        if coordinates.y < -63.99999999 && coordinates.y > -91.42857133 {
+            
+        }
+        if coordinates.y < -91.42857133 && coordinates.y > -118.8571428 {
+            
+        }
+        if coordinates.y < -118.8571428 && coordinates.y > -146.2857142 {
+            
+        }
+        if coordinates.y < -146.2857142 && coordinates.y > -173.7142856 {
+            
+        }
+        if coordinates.y < -173.7142856 && coordinates.y > -201.142857 {
+            
+        }
+        if coordinates.y < -201.142857 && coordinates.y > -228.5714284 {
+            
+        }
+        if coordinates.y < -228.5714284 && coordinates.y > -255.9999999 {
+            
+        }
+        if coordinates.y < -255.9999999 && coordinates.y > -283.425713 {
+            
+        }
+        if coordinates.y < -283.425713 && coordinates.y > -310.8571427 {
+            
+        }
+        if coordinates.y < -310.8571427 && coordinates.y > -338.2857141 {
+            
+        }
+        
         
     }
     if coordinates.x > -356.5714286 && coordinates.x <  -329.1428571 {
