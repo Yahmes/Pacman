d577a45c9a611bbaa184e7a37d4254eaf02840bb
Horge <mattk@ZPR-L0007089.local>
2017-12-05 09:03:41 -0500

    grid detection x-axis done

--- Pacman/grid detection.swift
+++ Pacman/grid detection.swift
@@ -12,6 +12,93 @@ import GameplayKit
 
 func GridDetection(coordinates: CGPoint) -> CGPoint {
     var tile: CGPoint
+    var gridX: Double = 27.4285714286
+    var gridY: Double = 27.428571424
+    
+    if coordinates.x > -384 && coordinates.x < -356.5714286 {
+        
+    }
+    if coordinates.x > -356.5714286 && coordinates.x <  -329.1428571 {
+        
+    }
+    if coordinates.x > -329.1428571 && coordinates.x <  -301.7142857 {
+        
+    }
+    if coordinates.x > -301.7142857 && coordinates.x <  -274.2857143 {
+        
+    }
+    if coordinates.x > -274.2857143 && coordinates.x <  -246.8571429 {
+        
+    }
+    if coordinates.x > -246.8571429 && coordinates.x <  -219.4285714 {
+        
+    }
+    if coordinates.x > -219.4285714 && coordinates.x <  -192 {
+        
+    }
+    if coordinates.x > -192 && coordinates.x <  -164.5714286 {
+        
+    }
+    if coordinates.x > -164.5714286 && coordinates.x <  -137.1428571 {
+        
+    }
+    if coordinates.x > -137.1428571 && coordinates.x <  -109.7142857 {
+        
+    }
+    if coordinates.x > -109.7142857  && coordinates.x <  -82.28571429 {
+        
+    }
+    if coordinates.x > -82.28571429 && coordinates.x <  -54.85714286 {
+        
+    }
+    if coordinates.x > -54.85714286 && coordinates.x <  -27.42857143{
+        
+    }
+    if coordinates.x > -27.42857143 && coordinates.x <  0 {
+        
+    }
+    if coordinates.x > 0 && coordinates.x <  27.42857143 {
+        
+    }
+    if coordinates.x > 27.42857143 && coordinates.x <  54.85714286 {
+        
+    }
+    if coordinates.x > 54.85714286  && coordinates.x <  82.28571429 {
+        
+    }
+    if coordinates.x > 82.28571429 && coordinates.x <  109.7142857 {
+        
+    }
+    if coordinates.x > 109.7142857 && coordinates.x <  137.1428571 {
+        
+    }
+    if coordinates.x > 137.1428571 && coordinates.x <  164.5714286 {
+        
+    }
+    if coordinates.x > 164.5714286 && coordinates.x <  192 {
+        
+    }
+    if coordinates.x > 192 && coordinates.x <  219.285714 {
+        
+    }
+    if coordinates.x > 219.285714 && coordinates.x <  246.8571429 {
+        
+    }
+    if coordinates.x > 246.8571429 && coordinates.x <  274.857143 {
+        
+    }
+    if coordinates.x > 274.857143 && coordinates.x <  301.7142857 {
+        
+    }
+    if coordinates.x > 301.7142857 && coordinates.x <  329.1428571 {
+        
+    }
+    if coordinates.x > 329.1428571 && coordinates.x <  356.5714286 {
+        
+    }
+    if coordinates.x > 356.5714286 && coordinates.x <  384 {
+        
+    }
     
     return tile
 }
