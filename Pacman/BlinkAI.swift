35894204a13510e9e2966fded03b721b5cf96c1d
SSFanatic <murrm8106@wrdsb.ca>
2017-12-08 09:21:08 -0500

    AI Stuff

---
+++ Pacman/BlinkAI.swift
@@ -0,0 +1,18 @@
+//
+//  BlinkAI.swift
+//  Pacman
+//
+//  Created by Matthew Murray on 2017-12-08.
+//  Copyright © 2017 Robit-Team  +1. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+ var blinky = SKSpriteNode(imageNamed: "blinky+left")
+
+blinky.position = CenterOfTile(tile: CGPoint(x: 14, y: 14))
+blinky.xScale = 0.3
+blinky.yScale = 0.3
+addChild(blinky)
---
+++ Pacman/PinkyAI.swift
@@ -0,0 +1,20 @@
+//
+//  PinkyAI.swift
+//  Pacman
+//
+//  Created by Matthew Murray on 2017-12-08.
+//  Copyright © 2017 Robit-Team  +1. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+
+var pinky = SKSpriteNode(imageNamed: "pinky+left")
+
+pinky.position = CenterOfTile(tile: CGPoint(x: 13, y: 14))
+pinky.xScale = 0.3
+pinky.yScale = 0.3
+addChild(pinky)
+
